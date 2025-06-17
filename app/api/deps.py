a01f4d2d087c679bdbda50b6fbcae187736c60f3
HHamlet <hh.harutyunyan@mail.ru>
2025-06-18 00:08:45 +0400

    change get_current_user

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
 from typing import AsyncGenerator
-from fastapi import Depends, HTTPException, status, Cookie
+from fastapi import Depends, HTTPException, status, Request
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
@@ -9,6 +9,7 @@ from app.core.config import settings
 from app.core.security import verify_access_token
 from app.core.session import async_session
 from app.models.user import UserModel
+from fastapi.security.utils import get_authorization_scheme_param
 
 reusable_oauth2 = OAuth2PasswordBearer(tokenUrl=f"{settings.API_V1_STR}/auth/login")
 
@@ -18,14 +19,15 @@ async def get_async_db() -> AsyncGenerator[AsyncSession, None]:
         yield session
 
 
-async def get_current_user(db: AsyncSession = Depends(get_async_db),
-                           token: str = Depends(reusable_oauth2),
-                           access_token_cookie: str = Cookie(None)) -> UserModel:
+async def get_current_user(request: Request, db: AsyncSession = Depends(get_async_db),):
 
-    if not token and access_token_cookie:
-        token = access_token_cookie.replace("Bearer ", "")
-    if not token:
-        raise HTTPException(status_code=401, detail="Not authenticated")
+    auth_header = request.headers.get("Authorization")
+    scheme, token = get_authorization_scheme_param(auth_header)
+    if scheme.lower() != "bearer" or not token:
+        token = request.cookies.get("access_token")
+        print(f"access_token {token}")
+        if not token:
+            raise HTTPException(status_code=401, detail="Not authenticated")
 
     token_data = verify_access_token(token)
     if token_data.exp < datetime.now(timezone.utc):
@@ -36,7 +38,6 @@ async def get_current_user(db: AsyncSession = Depends(get_async_db),
         )
     result = await db.execute(select(UserModel).filter(UserModel.id == int(token_data.sub)))
     user = result.scalars().first()
-
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
