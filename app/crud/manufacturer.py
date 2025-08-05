fc66e6f59e97b3b7e7f4e2edf85a3eb75aeefc4a
HHamlet <hh.harutyunyan@mail.ru>
2025-08-05 17:07:23 +0400

    CRUD manufacturer rev.01

--- app/crud/manufacturer.py
+++ app/crud/manufacturer.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from fastapi import Depends, HTTPException, Response, status
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 from app.api.deps import get_async_db, get_current_active_admin
@@ -16,15 +16,42 @@ async def get_all_manufacturers(db: AsyncSession):
     return result.unique().scalars().all()
 
 
-async def read_manufacturers(paginate: paginate_dep, db: AsyncSession = Depends(get_async_db),):
-
+async def get_manufacturers(paginate: paginate_dep, db: AsyncSession = Depends(get_async_db),):
     offset = (paginate.page - 1) * paginate.per_page
-
-    result = await db.execute(select(ManufacturerModel).limit(int(paginate.per_page)).offset(offset))
+    total_result = await db.execute(select(func.count()).select_from(ManufacturerModel))
+    total = total_result.scalar()
+    result = await db.execute(select(ManufacturerModel).limit(paginate.per_page).offset(offset))
     manufacturers = result.unique().scalars().all()
-    dict_manufacturer = [await object_as_dict(manufacturer) for manufacturer in manufacturers]
+    dict_manufacturers = [await object_as_dict(manufacturer) for manufacturer in manufacturers]
 
-    return dict_manufacturer
+    return {"manufacturers": dict_manufacturers,
+            "total": total,
+            "page": paginate.page,
+            "per_page": paginate.per_page}
+
+
+async def get_manufacturer_by_name(name: str, db: AsyncSession = Depends(get_async_db),):
+
+    result = await db.execute(select(ManufacturerModel).
+                              options(selectinload(ManufacturerModel.parts)).
+                              filter(ManufacturerModel.name == name))
+    manufacturer = result.scalars().first()
+
+    if not manufacturer:
+        raise HTTPException(status_code=404, detail="Manufacturer not found",)
+    return manufacturer
+
+
+async def get_manufacturer_by_id(manufacturer_id: int, db: AsyncSession = Depends(get_async_db),):
+
+    result = await db.execute(select(ManufacturerModel).
+                              options(selectinload(ManufacturerModel.parts)).
+                              filter(ManufacturerModel.id == manufacturer_id))
+    manufacturer = result.scalars().first()
+
+    if not manufacturer:
+        raise HTTPException(status_code=404, detail="Manufacturer not found",)
+    return manufacturer
 
 
 async def create_manufacturer(manufacturer_in: ManufacturerCreate, db: AsyncSession = Depends(get_async_db),
@@ -41,18 +68,6 @@ async def create_manufacturer(manufacturer_in: ManufacturerCreate, db: AsyncSess
     db.add(manufacturer)
     await db.commit()
     await db.refresh(manufacturer)
-    return manufacturer
-
-
-async def read_manufacturer(manufacturer_id: int, db: AsyncSession = Depends(get_async_db),):
-
-    result = await db.execute(select(ManufacturerModel).
-                              options(selectinload(ManufacturerModel.parts)).
-                              filter(ManufacturerModel.id == manufacturer_id))
-    manufacturer = result.scalars().first()
-
-    if not manufacturer:
-        raise HTTPException(status_code=404, detail="Manufacturer not found",)
     return manufacturer
 
 
