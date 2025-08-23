78111d7554d60328b93d9b964ef5eb8b6aef343a
HHamlet <hh.harutyunyan@mail.ru>
2025-08-23 17:17:04 +0400

    init corr

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,12 +1,16 @@
+from .base import BaseModel
+
 from .user import UserModel
 from .order import OrderModel
-from .base import BaseModel
 from .order_item import OrderItemModel
 from .car import CarModel
 from .part import PartModel
 from .category import CategoryModel
 from .warehouse import WarehouseModel
+from .warehouse_part import WarehousePartModel
 from .manufacturer import ManufacturerModel
 
-__all__ = ["UserModel", "OrderModel", "BaseModel", "OrderItemModel",
-           "CarModel", "PartModel", "CategoryModel", "WarehouseModel", "ManufacturerModel"]
+__all__ = [
+    "UserModel", "OrderModel", "OrderItemModel", "CarModel", "PartModel",
+    "CategoryModel", "WarehouseModel", "WarehousePartModel", "ManufacturerModel", "BaseModel"
+]
