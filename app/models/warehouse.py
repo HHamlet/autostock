e59d9c131c0dac8d87e4f763a8274b2566df401a
HHamlet <hh.harutyunyan@mail.ru>
2025-08-23 13:41:57 +0400

    corr warehouse model

--- app/models/warehouse.py
+++ app/models/warehouse.py
@@ -1,7 +1,5 @@
-from sqlalchemy import ForeignKey, Table, Column, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.models.base import BaseModel
-from app.models.part import PartModel
 
 
 class WarehouseModel(BaseModel):
@@ -10,18 +8,8 @@ class WarehouseModel(BaseModel):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     name: Mapped[str]
     location: Mapped[str]
-    # part_id: Mapped[int] = mapped_column(ForeignKey("parts.id"))
-    #
-    # parts: Mapped[list["PartModel"]] = relationship(
-    #     "PartModel", secondary="warehouse_part", back_populates="warehouses", lazy="joined")
+
+    warehouse_parts: Mapped[list["WarehousePartModel"]] = relationship("WarehousePartModel", back_populates="warehouse")
 
     def __repr__(self):
         return f"WarehouseModel({self.name}, Location: {self.location})"
-
-
-warehouse_part = Table(
-    "warehouse_part",
-    BaseModel.metadata,
-    Column("warehouse_id", ForeignKey("warehouses.id"), primary_key=True),
-    Column("part_id", ForeignKey("parts.id"), primary_key=True),
-    Column("quantity", Integer, nullable=False, default=0),)
